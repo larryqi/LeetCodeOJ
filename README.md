975e145d43aa6c5d3395942ca640218a1cbd26d5
linqi <qilinsjtu@gmail.com>
2015-08-18 20:31:07 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## LeetCode Online Judge
+
+* [Introduction](#introduction)
+
+### Introduction
+[LeetCode OJ](http://leetcode.com) is a platform for preparing technical coding interviews. Pick from an expanding library of more than
+190 questions, code and submit your solution to see if you have solved it correctly. It is that easy!
---
+++ src/Sum3.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by linqi on 15/8/3.
+ */
+class Solution {
+    public static List<List<Integer>> threeSum(int[] nums) {
+        List<Integer> tmp = new ArrayList<>();
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+
+
+        return result;
+
+    }
+}
+public class Sum3 {
+    public static void main(String[] args) {
+        int[] nums = {-1,0,1,2,-1,-4};
+        System.out.println(nums[0] % 3);
+
+        //new Solution().threeSum(nums).forEach(System.out::println);
+
+    }
+}
