2a7a957ca9e5c1a3a8e03d1b7b3cbbc3e3269929
larryqi <qilinsjtu@gmail.com>
2015-08-23 16:43:39 +0800

    add 3Sum(https://leetcode.com/problems/3sum/)

--- src/Sum3.java
+++ src/Sum3.java
@@ -7,9 +7,45 @@ import java.util.List;
  */
 class Solution {
     public static List<List<Integer>> threeSum(int[] nums) {
-        List<Integer> tmp = new ArrayList<>();
         List<List<Integer>> result = new ArrayList<>();
+        if (nums.length < 3) {
+            return result;
+        }
+        // sort array
         Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i == 0 || nums[i] > nums[i-1]) {
+                int negate = 0 - nums[i];
+                int start = i + 1;
+                int end = nums.length - 1;
+                while ( start < end) {
+                    if (nums[start] + nums[end] == negate) {
+                        ArrayList<Integer> tmp = new ArrayList<>();
+                        tmp.add(nums[i]);
+                        tmp.add(nums[start]);
+                        tmp.add(nums[end]);
+                        result.add(tmp);
+                        start++;
+                        end--;
+                        while (start < end && nums[end] == nums[end+1]) {
+                            end--;
+                        }
+                        while (start < end && nums[start] == nums[start-1]) {
+                            start++;
+                        }
+                    }
+                    else if (nums[start] + nums[end] < negate) {
+                        start++;
+                    }
+                    else {
+                        end--;
+                    }
+                }
+
+                }
+
+            }
+
 
 
         return result;
@@ -19,9 +55,7 @@ class Solution {
 public class Sum3 {
     public static void main(String[] args) {
         int[] nums = {-1,0,1,2,-1,-4};
-        System.out.println(nums[0] % 3);
-
-        //new Solution().threeSum(nums).forEach(System.out::println);
+        new Solution().threeSum(nums).forEach(System.out::println);
 
     }
 }
